8175b3b5770be6ebf92fa306d63fc2090fe4a90b
Alex Correa <alexcorrea31@outlook.com>
2025-11-12 15:38:11 -0500

    Initial commit - expense tracker project setup

--- app.py
+++ app.py
@@ -136,5 +136,5 @@ def index():
 if __name__ == '__main__':
     init_db()
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=port)
     print('app running')
